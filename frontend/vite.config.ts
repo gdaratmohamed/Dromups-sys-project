401b39bbcad2453c90b0f311277641c9b0d4d295
Gdarat Mohamed <gdaratmohamed73@gmail.com>
2025-08-13 14:58:52 +0100

    test stage

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  }
+    plugins: [react()],
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './src/setupTests.js'
+    }
 })
